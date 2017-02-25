ea9c06dbc6e6ad997a7a51a8c175e4728cf66a59
Nitish Dayal <nitishdayal@outlook.com>
2017-02-24 19:27:39 -0800

    Updated test suite

--- __tests__/alder.spec.js
+++ __tests__/alder.spec.js
@@ -1,7 +1,11 @@
 const child_process = require('child_process')
+const path = require('path')
+
+const alder = path.resolve(__dirname, './../alder.js')
+
 
 const execute = (args, cb) => {
-  child_process.exec(`./alder.js ${args}`, {}, (err, stdout, stderr) => {
+  child_process.exec(`${alder} fixtures ${args}`, {}, (err, stdout, stderr) => {
     if (err) {
       throw err
     }
@@ -11,24 +15,55 @@ const execute = (args, cb) => {
 }
 
 describe('alder', () => {
-  [
-    '',
-    '-a',
-    '-d 1',
-    '-D',
-    '-e bar',
-    '-f',
-    '-i',
-    '-I',
-    '-p bar',
-    '-s',
-    '-t',
-    '--prune',
-    '--filelimit=3',
-    '--jsx'
-  ].map(flag => {
-    it(`works with the '${flag}' flag`, (done) => {
-      execute(`fixtures ${flag}`, done)
-    })
+  it(`works with 0 arguments/flags`, (done) => {
+    execute(``, done)
+  })
+
+  it(`only renders the tree to a depth of 1`, (done) => {
+    execute(`--depth 1`, done) && execute(` -d 1`, done)
+  })
+
+  it(`only prints directories`, (done) => {
+    execute(`-D`, done) && execute(` --directories`, done)
+  })
+
+  it(`excludes files matching pattern (bar)`, (done) => {
+    execute(`-e bar`, done) && execute(`--exclude bar`, done)
+  })
+
+  it(`prints the full path prefix for each file`, (done) => {
+    execute(`-f`, done) && execute(`--full`, done)
+  })
+
+  it(`will not print indentation lines`, (done) => {
+    execute(`-i`, done) && execute(`--no-indent`)
+  })
+
+  it(`ignores files listed in .gitignore (foo folder)`, (done) => {
+    execute(`-I`, done) && execute(` --git-ignore`, done)
+  })
+
+  it(`will only show files matching pattern (foo)`, (done) => {
+    execute(`-p foo`, done) && execute(`--include foo`)
+  })
+
+  it(`shows file size in tree`, (done) => {
+    execute(`-s`, done) && execute(`--sizes`)
+  })
+
+  it(`prints the last modified date for each file`, (done) => {
+    execute(`-t`, done) && execute(` --time-stamp`, done)
+  })
+
+  it(`prunes empty directories from output`, (done) => {
+    execute(`--prune`, done)
+  })
+
+  it(`only descends directories with <2 files`, (done) => {
+    execute(`--filelimit 2`, done)
+  })
+
+  it(`makes everything all jsxexy`, (done) => {
+    execute(`--jsx`, done)
   })
 })
